4b9f7e03f0447d9f17d3a6f6fef67e894bb6e8c2
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 12:23:41 -0400

    Fixed source methods and a few package methods

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,39 @@
 from conans import ConanFile, tools, os
 
+
 class BoostIoConan(ConanFile):
     name = "Boost.Io"
     version = "1.64.0"
+    generators = "boost"
+    settings = "os", "arch", "compiler", "build_type"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-io"
-    source_url = "https://github.com/boostorg/io"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["io"]
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    build_requires = "Boost.Generator/0.0.1@bincrafters/testing"
     requires = "Boost.Config/1.64.0@bincrafters/testing"
 
     def source(self):
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + self.version  
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name)) 
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
+
+    def build(self):
+        self.run(self.deps_user_info['Boost.Generator'].b2_command)
 
     def package(self):
+        self.copy(pattern="*", dst="lib", src="stage/lib")
         for lib_short_name in self.lib_short_names:
             include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)		
+            self.copy(pattern="*", dst="include", src=include_dir)
 
-    def package_id(self):
-        self.info.header_only()
+    def package_info(self):
+        self.user_info.lib_short_names = ",".join(self.lib_short_names)
+        self.cpp_info.libs = self.collect_libs()
+        self.cpp_info.defines.append("BOOST_ALL_NO_LIB=1")

f2d26335dbacf3a0a7cfa60b6496c5a40a21bd02
Jerry W <jerrywiltse@gmail.com>
2017-09-19 16:27:41 -0400

    changed refs to stable

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class BoostIoConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["io"]
-    requires = "Boost.Config/1.65.1@bincrafters/testing"
+    requires = "Boost.Config/1.65.1@bincrafters/stable"
 
     def source(self):
         boostorg_github = "https://github.com/boostorg"

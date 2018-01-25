a1b36ad660270edd2454f54c082ffab47bca8ee5
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:11:54 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostIoConan(ConanFile):
     name = "boost_io"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_io"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["io"]
     is_header_only = True
 
